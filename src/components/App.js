1861b2ebf9143f14d85bcc0b39508c3d18fabecd
AlejaMC15 <aleja-mc@hotmail.com>
2020-04-07 23:08:23 -0500

    se modifico el random

--- src/components/App.js
+++ src/components/App.js
@@ -3,45 +3,53 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './Styles/index.css';
 import imgTwitter from '../images/twitter.png';
 import imgIn from '../images/linkedin.png';
-import Ciclo from './ciclo'
+import Ciclo from './ciclo';
 
 class Quote extends React.Component {
     constructor(props) {
         super(props);
 
+        this.colors = ['red', 'blue', 'black', 'yellow', 'violet', 'pink', 'green', 'gray', 'purple', 'brown'];
+
+
         this.state = {
-            quotes: true,
+            color: '',
         };
+
+        this.generateRandomcolor = this.generateRandomcolor.bind(this);
+    }
+
+
+    generateRandomcolor() {
+        const random = Math.round(Math.random() * this.colors.length - 1)
+        this.setState({ color: this.colors[random] })
     }
 
     render() {
-        const { quotes } = this.state;
+        const { color } = this.state;
         return (
-
-            <div className="container-fluid d-flex justify-content-center align-item-center p-3">
-                <div className="row" id="minh-100" >
-                    <div className="col-sm-8 align-items-center">
-                        <div className="card bg-white text-info text-center p-5">
-                            <blockquote className="blockquote mb-0 p-3">
-                                <p className="textQuote">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat.</p>
-                                <footer className="blockquote-footer text-info">
-                                    <small className="textTwoQuote">
-                                    Someone famous in <cite className="textThreQuote" title="Source Title">Source Title</cite>
-                                    </small>
-                                </footer>
-                            </blockquote>
-                            <div className="row">
-                                <div className="col d-flex bd-highlight mb-0 justify-content-center">
-                                    <button type="button" className="btn btn-ligth"><img className="icono mg-2 bg-white" src={imgTwitter} /></button>
-                                    <button type="button" className="btn btn-ligth"><img className="icono mg-2 bg-white" src={imgIn} /></button>
-
-                                        {quotes &&
-                                        <Ciclo />
-                                        }
-
-                                    <button type="button" className="btn btn-info ml-auto bd-highlight"
-                                        onClick={() => this.setState({quotes: !quotes})}
-                                    >Nueva Cita</button>
+            <div style={{ background: color }}>
+                <div className="container-fluid d-flex justify-content-center align-item-center p-3">
+                    <div className="row" id="minh-100" >
+                        <div className="col-sm-8 align-items-center p-2">
+                            <div className="card bg-white text-info text-center p-4">
+                                {/* <blockquote className="blockquote mb-0 p-3">
+                                    <p className="textQuote">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat.</p>
+                                    <footer className="blockquote-footer text-info">
+                                        <small className="textTwoQuote">
+                                        Someone famous in <cite className="textThreQuote" title="Source Title">Source Title</cite>
+                                        </small>
+                                    </footer>
+                                </blockquote> */}
+                                <Ciclo color={color}  randomColor={this.generateRandomcolor} />
+                                <div className="row">
+                                    <div className="col d-flex bd-highlight mb-0 p-3 justify-content-center">
+                                        <button type="button" className="btn btn-ligth"><a href="https://twitter.com/explore" target="_blank"></a><img className="icono mg-2 bg-white" src={imgTwitter} /></button>
+                                        <button type="button" className="btn btn-ligth"><a href="https://www.linkedin.com/home" target="_blank"></a><img className="icono mg-2 bg-white" src={imgIn} /></button>
+                                        <button type="button" id="boton" className="btn btn-info ml-auto bd-highlight" style={{ background: color }}
+                                            onClick={this.generateRandomcolor}
+                                        >Nueva Cita</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -49,10 +57,9 @@ class Quote extends React.Component {
                 </div>
             </div>
 
-        )
+        );
     }
 
 }
 
 export default Quote;
-
--- src/components/ciclo.js
+++ src/components/ciclo.js
@@ -5,33 +5,97 @@ class Ciclo extends React.Component {
     constructor(props) {
         super(props);
 
+        this.frases = [
+            {
+                frase: '“Para liberarse, la mujer debe sentirse libre, no para rivalizar con los hombres, sino libres en sus capacidades y personalidad”.',
+                autor: 'Indira Gandhi',
+                ano: '1917-1984'
+            },
+            {
+                frase: '“La vida es corta: sonríele a quien llora, ignora a quien te critica, y sé feliz con quien te importa”.',
+                autor: 'Marilyn Monroe',
+                ano: '1926-1962'
+            },
+            {
+                frase: '“Usted no puede esperar construir un mundo mejor sin mejorar a las personas. Cada uno de nosotros debe trabajar para su propia mejora”.',
+                autor: 'Marie Curie',
+                ano: '1867-1934'
+            },
+            {
+                frase: '“No nacemos como mujer, sino que nos convertimos en una”.',
+                autor: 'Simone de Beavoir',
+                ano: '1908-1986'
+            },
+            {
+                frase: '“La ceguera nos separa de las cosas que nos rodea, pero la sordera nos separa de las personas”.',
+                autor: 'Hellen Keller',
+                ano: '1880-1968'
+            },
+            {
+                frase: '“El mayor peligro que nos depara el futuro es la apatía”.',
+                autor: 'Jane Goodall',
+                ano: '1934'
+            },
+            {
+                frase: '“Son nuestras decisiones las que muestran quiénes somos realmente, más que nuestras habilidades”.',
+                autor: 'J.K. Rowling',
+                ano: '1965'
+            },
+            {
+                frase: '“Debemos decirles a nuestras jóvenes que sus voces son importantes”.',
+                autor: 'Malala Yousafzai',
+                ano: '1997'
+            },
+            {
+                frase: 'Nada es imposible, la propia palabra lo dice: “puedo hacerlo”.',
+                autor: 'Audrey Hepburn',
+                ano: '1929-1993'
+            },
+            {
+                frase: '“La paz comienza con una sonrisa”.',
+                autor: 'Madre Teresa de Calcuta',
+                ano: '1910-1997'
+            }
+
+        ];
+
         this.state = {
-
+            quota: '',
         };
-    }
 
+        this.selectRandomQuota = this.selectRandomQuota.bind(this);
+    }
 
     //Montaje
     componentDidMount() {
-
-
+        const { randomColor } = this.props;
+        randomColor();
     }
 
-    //Desmontaje
-    componentWillMount() {
-
-
+    // Actualizacion
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.color !== this.props.color) {
+            this.selectRandomQuota();
+        }
     }
+
+    selectRandomQuota() {
+        const random = Math.round(Math.random() * this.frases.length - 1)
+        this.setState({ quota: this.frases[random] });
+    }
+
 
     render() {
+        const { quota } = this.state;
+        const { color } = this.props;
         return (
 
-            <div>
+            <div style={{ color }}>
                 <div>
-                    “Para liberarse, la mujer debe sentirse libre, no para rivalizar con los hombres, sino libres en sus capacidades y personalidad”.
+                    “{quota.frase}”.
                 </div>
                 <div>
-                    Indira Gandhi / 1917-1984
+                    {quota.autor} / {quota.ano}
                 </div>
             </div>
 
